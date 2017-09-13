2de35ce550fce0232e35d324d43d6d00ac5e10f2
Neycker Aguayo <neyaldrichdev@outlook.com>
2017-09-13 16:58:27 -0500

    Login y AdminLogin Activities

---
+++ app/src/main/java/com/sinergiass/asistencia/LoginActivity.java
@@ -0,0 +1,50 @@
+package com.sinergiass.asistencia;
+
+import android.content.Intent;
+import android.location.Location;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
+
+/**
+ * Created by avera on 13/09/17.
+ */
+
+public class LoginActivity extends AppCompatActivity {
+
+    private Button operador;
+    private Button admin;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        toolbar.setTitle("Bienvenido");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+        operador = (Button)findViewById(R.id.operador);
+        admin = (Button)findViewById(R.id.admin);
+
+
+        admin.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent(v.getContext(), LoginAdminActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
