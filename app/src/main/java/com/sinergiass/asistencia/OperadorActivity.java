a3d8c9c20e116503ebba28a1983236858c11d4e6
julio9319 <juliohbk_93@hotmail.es>
2017-09-20 17:34:36 -0500

    Sprint#2 Task "Crear Formulario De ingreso del operador con guardado base local y agregar a la listView"

--- app/src/main/java/com/sinergiass/asistencia/OperadorActivity.java
+++ app/src/main/java/com/sinergiass/asistencia/OperadorActivity.java
@@ -7,8 +7,11 @@ import android.os.Bundle;
 import android.util.Base64;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.sinergiass.asistencia.model.Operador;
 import com.sinergiass.asistencia.ws.FaceDetectorWS;
 
 import java.io.ByteArrayOutputStream;
@@ -16,13 +19,23 @@ import java.util.Calendar;
 import java.util.Date;
 
 public class OperadorActivity extends AppCompatActivity {
-    Button btnFace;
+    Button btnFace,guardar;
+    TextView nombres, apellidos, cedula, telefono;
+    Operador operador;
+    String encodedImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_operador);
+
         btnFace = (Button) findViewById(R.id.btnFace);
+        guardar = (Button) findViewById(R.id.guardar);
+        nombres = (TextView) findViewById(R.id.txt_nombres);
+        apellidos = (TextView) findViewById(R.id.txt_apellidos);
+        cedula = (TextView) findViewById(R.id.txt_cedula);
+        telefono = (TextView) findViewById(R.id.txt_telefono);
+
         btnFace.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -30,6 +43,24 @@ public class OperadorActivity extends AppCompatActivity {
                 startActivityForResult(intent, 0);
             }
         });
+
+        guardar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(OperadorActivity.this, MainActivity.class);
+                operador = new Operador();
+                operador.setNombre(nombres.getText().toString());
+                operador.setApellido(apellidos.getText().toString());
+                operador.setCedula(cedula.getText().toString());
+                operador.setTelefono(telefono.getText().toString());
+                operador.setDatosCara(encodedImage);
+                operador.save();
+                startActivity(intent);
+                Toast.makeText(OperadorActivity.this, "Guardado Exitoso!", Toast.LENGTH_LONG).show();
+            }
+        });
+
+
     }
 
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
@@ -39,7 +70,7 @@ public class OperadorActivity extends AppCompatActivity {
             ByteArrayOutputStream baos = new ByteArrayOutputStream();
             imageBitmap.compress(Bitmap.CompressFormat.PNG, 100, baos);
             byte[] imagenByte = baos.toByteArray();
-            String encodedImage = Base64.encodeToString(imagenByte, Base64.DEFAULT);
+            encodedImage = Base64.encodeToString(imagenByte, Base64.DEFAULT);
 
             new FaceDetectorWS().enviaImagen(encodedImage,"1");
 
